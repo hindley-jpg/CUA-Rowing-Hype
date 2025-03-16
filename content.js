ac2c6e3dd856c947ebc5f16073a54d2c6a9c8380
Claudia Hindley <hindley@cua.edu>
2025-03-16 09:05:55 -0400

    new practice files (not for publishing)

---
+++ content.js
@@ -0,0 +1,49 @@
+const data = {
+    mainImage: {
+        imgSrc:"images\IMG_8739_exported_9335.jpg",
+        id:"sunrise row"
+    },
+    poemText:{
+        id:"poem",
+        text:`'T is not too late to seek a newer world. 
+            Push off, and sitting well in order smite
+            The sounding furrows; for my purpose holds
+            To sail beyond the sunset, and the baths
+            Of all the western stars, until I die.
+            It may be that the gulfs will wash us down,
+            It may be we shall touch the Happy Isles,
+            And see the great Achilles, whom we knew.
+            Tho' much is taken, much abides; and tho'
+            We are not now that strength which in old days
+            Moved earth and heaven, that which we are, we are;
+            One equal temper of heroic hearts,<br>
+            Made weak by time and fate, but strong in will`,
+    },
+    aboutIntro:{
+        id:"about",
+        title:"about",
+        text:`Catholic University Women's Rowing is a crew of highly motivated athletes dedicated to re-writing our limits
+                on land or water. At CUA Rowing Hype, we bring together the resources for CUA rowers and fans to track our team's progress through another epic season!`,
+    },
+    racesSection1:{
+        title:"Upcoming Races",
+        article:{
+            title:"Dad Vail Regatta",
+            articleContent:{
+                imgSrc:"images\dad_vail_logo.png",
+                link1:{
+                    href:"https://www.regattacentral.com/v3/cms/regatta/9412/heat_sheet",
+                    text: "Schedule",
+                },
+                link2:{
+                    href:"https://www.regattacentral.com/regatta/entries/competitors?job_id=9412&event_id=17",
+                    text:"Lineups",
+                },
+                link3:{
+                    href:"",
+                    text:"livestream",
+                },
+            },
+        },
+    },
+}
