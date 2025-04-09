d74e47eb8f7002d3a226fd54bdfc7e77f24ed743
Claudia Hindley <hindley@cua.edu>
2025-04-09 19:01:44 -0400

    schuylkill gallery, javascript

--- scripts/content.js
+++ scripts/content.js
@@ -1,3 +1,56 @@
+const gallery = document.getElementById("gallery")
+const modal1 = document.getElementById("myModal1")
+const modal2 = document.getElementById("myModal2")
+gallery.addEventListener("click", function(Event) {
+  if (Event.target.id === 'open1') {
+    // Open Modal 1
+    modal1.style.display ='block';
+  } else if (Event.target.id === 'open2') {
+    // Open Modal 2
+    modal2.style.display = 'block';
+  }
+});
+
+
+document.querySelectorAll('.xButton').forEach(button => {
+  button.addEventListener('click', function() {
+    closeModals();
+  });
+});
+
+// Function to close all modals
+function closeModals() {
+  modal1.style.display = 'none';
+  modal2.style.display = 'none';
+
+}
+
+
+
+
+
+
+/*const modals = document.querySelectorAll(".gallery_modal");
+ 
+const open = document.querySelectorAll(".open");
+const close = document.querySelectorAll(".xButton");
+
+open.forEach(button => {
+  button.addEventListener('click', () => {
+    const modalId = button.getAttribute('data-modal');
+    const modal = document.getElementById(modalId);
+    modal.style.display = 'block';
+  });
+});
+
+modals.forEach(modal => {
+  const closeButton = modal.querySelector('.xButton');
+  closeButton.addEventListener('click', () => {
+    modal.style.display = 'none';
+  });
+});
+
+
 const modal = document.getElementById("myModal");
 const openButton = document.getElementById("open");
 const xButton = document.getElementById("x");
@@ -12,4 +65,4 @@ window.onclick = function(event) {
     if (event.target == xButton) {
       modal.style.display = "none";
     }
-}
+}*/
